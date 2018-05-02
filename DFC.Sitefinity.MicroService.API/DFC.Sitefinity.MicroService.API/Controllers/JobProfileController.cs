33167fdcb27034e802bf08fd0dbf95d6e6b639ce
Trevor Kapswarah <trevk15@yahoo.co.uk>
2018-05-02 12:34:36 +0100

    Adding Solution Git Repo

---
+++ DFC.Sitefinity.MicroService.API/DFC.Sitefinity.MicroService.API/Controllers/JobProfileController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DFC.Sitefinity.MicroService.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class JobProfileController : Controller
+    {
+        // GET api/jobprofile/plumber
+        [HttpGet("{urlname}")]
+        public IDictionary<string, string> Get(string urlName)
+        {
+            var result = new ConcurrentDictionary<string, string> ();
+
+            result.TryAdd("HowToBecome", "<div></div>");
+            result.TryAdd("EntryQualifications", "<div></div>");
+            result.TryAdd("WhatYouWillDo", "<div></div>");
+
+            return result;
+        }
+    }
+}
