feb05300ca4d5b416b8bf54a06b32c0ffa1eb089
Trevor Kapswarah <trevk15@yahoo.co.uk>
2018-05-08 16:21:20 +0100

    Unblocking Muthu

--- DFC.Sitefinity.MicroService.API/DFC.Sitefinity.MicroService.API/Repositories/Repository.cs
+++ DFC.Sitefinity.MicroService.API/DFC.Sitefinity.MicroService.API/Repositories/Repository.cs
@@ -1,33 +1,40 @@
 ﻿using System.Threading.Tasks;
 using DFC.Sitefinity.MicroService.API.Interfaces;
 using DFC.Sitefinity.MicroService.API.Models;
+using Microsoft.Extensions.Configuration;
 using MongoDB.Driver;
 
 namespace DFC.Sitefinity.MicroService.API.Repositories
 {
     public class Repository : IRepository
     {
+        private readonly IMongoDatabase mongoDatabase;
+        public Repository(IConfiguration configuration)
+        {
+            //var mongoCS = configuration["mongo-cs"];
+            //var client = new MongoClient(mongoCS);
+            //mongoDatabase = client.GetDatabase("SFMicroservices");
+        }
         public async Task<JobProfile> GetJobProfileByUrlNameAsync(string urlName)
         {
-            var client = new MongoClient();
-            var mongoDatabase = client.GetDatabase("SifefinityMicroService");
+            //var jobProfileCollection = mongoDatabase.GetCollection<JobProfile>("JobProfiles");
 
-            var jobProfileCollection = mongoDatabase.GetCollection<JobProfile>("JobProfiles");
+            //var result = await jobProfileCollection.FindAsync(doc => doc.UrlName.Equals(urlName));
 
-            var result = await jobProfileCollection.FindAsync(doc => doc.UrlName.Equals(urlName));
+            //return await result.FirstOrDefaultAsync();
 
-            return await result.FirstAsync();
+            return new JobProfile();
         }
 
         public async Task SaveJobProfileAsync(JobProfile jobProfile)
-        {
-            var client = new MongoClient();
-            var mongoDatabase = client.GetDatabase("SifefinityMicroService");
+        { 
+            //var jobProfileCollection = mongoDatabase.GetCollection<JobProfile>("JobProfiles");
 
-            var jobProfileCollection = mongoDatabase.GetCollection<JobProfile>("JobProfiles");
-
-             await jobProfileCollection.InsertOneAsync(jobProfile);
-
+            //var result = await jobProfileCollection.FindAsync(doc => doc.Id.Equals(jobProfile.Id)).Result.FirstOrDefaultAsync();
+            //if (result == null)
+            //{
+            //    await jobProfileCollection.InsertOneAsync(jobProfile);
+            //}
         }
     }
 }
